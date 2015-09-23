078a8b085f82155b6736ce7b6af90dc2ac594442
Gen1us2k <minkin.andrew@gmail.com>
2015-09-23 18:40:11 +0600

    Search fb posts by hashtag

---
+++ search.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_post_by_hashtag(hashtag):
+    url = "https://www.facebook.com/hashtag/%s" % hashtag
+    r = requests.get(url)
+    hashtags = []
+    for code in BeautifulSoup(r.content, 'html.parser').findAll('code'):
+        for div in BeautifulSoup(str(code.contents).replace('<!--', '').replace('-->', ''), 'html.parser').findAll('div', {'class': 'userContentWrapper'}):
+            userprofile = ""
+            user_content = ""
+            profile_link = ""
+            original_post = ""
+            data = BeautifulSoup(str(div), 'html.parser')
+            for span in data.findAll('span', {'class': 'fwb'}):
+                # print span
+                # print
+                # print
+                for link in span.findAll('a'):
+                    if not link:
+                        continue
+                    if link.get('href', ""):
+                        profile_link = link.get('href', "")
+                        userprofile = link.getText()
+
+            for span in data.findAll('span', {'class': 'fsm'}):
+                for link in span.findAll('a'):
+                    if not link:
+                        continue
+                    if link.get('href', ""):
+                        original_post = link.get('href', "")
+
+
+            for div in data.findAll('div', {'class': 'userContent'}):
+                user_content = div.getText()
+
+            hashtags.append({
+                'profile_link': profile_link,
+                'userprofile': userprofile,
+                'content': user_content,
+                'original_post': original_post
+                })
+    return hashtags
+
+
+
+if __name__ == '__main__':
+    print get_post_by_hashtag('делайдобро')
